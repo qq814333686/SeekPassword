5a731293ba1e48999b78bc0ad0c077952f979212
unknown <814333868@qq.com>
2020-05-19 11:36:23 +0800

    modified:   src/seek_password.js

--- src/seek_password.js
+++ src/seek_password.js
@@ -1,15 +1,15 @@
 // refer from https://flowerpassword.com
 function flower_password(pwd, key) {
 	var md5one = md5(pwd, key);
-	var md5two = md5(md5one, "snow");
-	var md5three = md5(md5one, "kise");
+	var md5two = md5(md5one, "sun");
+	var md5three = md5(md5one, "moon");
 	// to uppercase
 	var rule = md5three.split("");
 	var source = md5two.split("");
 	console.assert(rule.length === source.length, "md5 output length not equal");
 	for (var i = 0; i < source.length; ++i) {
 		if (isNaN(source[i])) {
-			var str = "sunlovesnow1990090127xykab";
+			var str = "starsintherain1995090127dream";
 			if (str.search(rule[i]) > -1) {
 				source[i] = source[i].toUpperCase();
 			}
@@ -20,7 +20,7 @@ function flower_password(pwd, key) {
 	if (isNaN(code1)) {
 		var code16 = code32.slice(0, 16);
 	} else {
-		var code16 = "K" + code32.slice(1, 16);
+		var code16 = "X" + code32.slice(1, 16);
 	}
 	return [code16, code32];
 }
@@ -30,10 +30,10 @@ function seek_password(hash) {
 	var lower = "abcdefghijklmnopqrstuvwxyz".split("");
 	var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 	var number = "0123456789".split("");
-	var punctuation = ",.:;!?".split("");
+	var punctuation = ",.-;!?".split("");
 	var alphabet = lower.concat(upper).concat(number).concat(punctuation);
 	// try to generate password
-	for (var i = 0; i <= hash.length - 10; ++i) {
+	for (var i = 0; i <= hash.length - 12; ++i) {
 		var sub_hash = hash.slice(i, i + 12).split("");
 		var count = 0;
 		var map_index = sub_hash.map(function(c) {
